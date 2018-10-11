b5e706477bec7f425ddd1e66f1055bd890dcaebf
Allen Muncy <Allen.W.Muncy@gmail.com>
2018-10-11 12:00:00 -0400

    Initial hex to rgb function

---
+++ hexToRgb.php
@@ -0,0 +1,5 @@
+<?php
+function hexToRgb($hex) {
+  list($r, $g, $b) = sscanf($hex, "#%02x%02x%02x");
+  return "$r, $g, $b";
+}
---
+++ rgbToHsl.php
@@ -0,0 +1,49 @@
+<?php
+  function rgbToHsl($r, $g, $b){
+    $r = $r / 255;
+    $g = $g / 255;
+    $b = $b / 255;
+
+    $max = max(array($r, $g, $b));
+    $min = min(array($r, $g, $b));
+
+
+
+    $h = $s = $l = ($max + $min) / 2;
+    if($max == $min) {
+      $h = 0;
+      $s = 0;
+    } else {
+      $d = $max - $min;
+
+      $s = $l > 0.5 ? $d / (2 - $max - $min) : $d / ($max + $min);
+
+      switch ($max) {
+        case $r:
+          $h = ($g - $b) / $d + ($g < $b ? 6 : 0);
+          break;
+        case $g: 
+          $h = ($b - $r) / $d + 2;
+          break;
+        case $b: 
+          $h = ($r - $g) / $d + 4;
+          break;
+
+      }
+
+      $h = $h / 6;
+    }
+    
+
+    $array = [($h*100+0.5)|0, (($s*100+0.5)|0) . '%', (($l*100+0.5)|0) . '%'];
+  
+    return $array[0] . ", " . $array[1] . ", " . $array[2];
+  }
+
+
+
+
+?>
+
+<h1 style="color: hsl(<?=rgbToHsl(184,64,25);?>);">Hello: HSL</h1>
+<h1 style="color: rgb(184,64,25);">Hello: RGB</h1>
